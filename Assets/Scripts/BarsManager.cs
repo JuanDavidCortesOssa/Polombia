836b730523f3ce6ec01f07dc0724d9ccdf3d1c48
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2021-10-03 16:28:57 -0500

    Setting up bars

---
+++ Assets/Scripts/BarsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Polombia
+{
+    public class BarsManager : MonoBehaviour
+    {
+        public Slider budget, support, approval;
+        public float animationTime;
+
+        private void Start()
+        {
+            GameManager.instance.OnBudgetChanged += UpdateBudgetBar;
+            GameManager.instance.OnSupportChanged += UpdateSupportBar;
+            GameManager.instance.OnApprovalChanged += UpdateApprovalBar;
+        }
+
+        public void UpdateBudgetBar(float value) =>
+            DOTween.To(() => budget.value, x => budget.value = x, value / 100, animationTime);
+
+        public void UpdateSupportBar(float value) =>
+            DOTween.To(() => support.value, x => support.value = x, value / 100, animationTime);
+
+        public void UpdateApprovalBar(float value) =>
+            DOTween.To(() => approval.value, x => approval.value = x, value / 100, animationTime);
+
+        private void OnDestroy()
+        {
+            GameManager.instance.OnBudgetChanged -= UpdateBudgetBar;
+            GameManager.instance.OnSupportChanged -= UpdateSupportBar;
+            GameManager.instance.OnApprovalChanged -= UpdateApprovalBar;
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
 
 namespace Polombia
 {
     public class GameManager : MonoBehaviour
     {
-        private List<Level> levels;
+        public static GameManager instance;
+
+        private List<Level> _levels;
+
+
+        #region bars
+
+        public event Action<float> OnBudgetChanged;
+        public event Action<float> OnSupportChanged;
+        public event Action<float> OnApprovalChanged;
+
+        private float _budget;
+        public float budget
+        {
+            set
+            {
+                _budget = Mathf.Clamp(value, 0, 100);
+                OnBudgetChanged.Invoke(_budget);
+            }
+            get { return _budget; }
+        }
+
+        private float _support;
+        public float support
+        {
+            set
+            {
+                _support = Mathf.Clamp(value, 0, 100);
+                OnSupportChanged.Invoke(_support);
+            }
+            get { return _support; }
+        }
+
+        private float _approval;
+        public float approval
+        {
+            set
+            {
+                _approval = Mathf.Clamp(value, 0, 100);
+                OnApprovalChanged.Invoke(_approval);
+            }
+            get { return _approval; }
+        }
+        #endregion
+
+        private void Awake()
+        {
+            if (GameManager.instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+        }
+
+        #region DebugBars
+
+        [Button]
+        public void ChangeBudget(float value) => budget = value;
+        [Button]
+        public void ChangeSupport(float value) => support = value;
+        [Button]
+        public void Changepproval(float value) => approval = value;
+
+        #endregion
     }
 }
