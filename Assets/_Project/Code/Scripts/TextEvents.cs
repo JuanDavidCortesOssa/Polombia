af6924647ccb0894b01e23ade83c380123b3f33f
JuanDavidCortesOssa <juan.cortns@uao.edu.co>
2021-11-18 02:41:24 -0500

    Speak animation improved

---
+++ Assets/_Project/Code/Scripts/TextEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextEvents : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public void StopAnimation()
+    {
+        animator.speed = 0;
+    }
+
+    public void StartAnimation()
+    {
+        animator.speed = 1f;
+    }
+}
